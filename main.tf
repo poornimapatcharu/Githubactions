f9eb8a3034b86819a6e7eb8224bdd2ac6a41a8f2
poornimapatcharu <poornimapatcharu@gmail.com>
2026-06-27 23:20:53 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket         = "poornima-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
+
 provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "nitnit"
-    }
+  ami           = "ami-0f88e80871fd81e91"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "nitnit"
+  }
 }
